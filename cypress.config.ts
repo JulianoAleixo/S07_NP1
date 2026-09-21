2423b3d4c63764464508902810f1e8675bd1f334
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2026-09-20 22:23:47 -0300

    chore: configuring cypress to make reports

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,26 +2,38 @@ import { defineConfig } from "cypress";
 import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
 import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
 import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
+import fixCypressOnFix from "cypress-on-fix";
 
 export default defineConfig({
-    e2e: {
-        baseUrl: "https://julianoaleixo.dev",
-        specPattern: "cypress/e2e/**/*.feature",
-        async setupNodeEvents(
-            on: Cypress.PluginEvents,
-            config: Cypress.PluginConfigOptions,
-        ) {
-            
-            await addCucumberPreprocessorPlugin(on, config);
-            
-            on(
-                "file:preprocessor",
-                createBundler({
-                    plugins: [createEsbuildPlugin(config)],
-                }),
-            );
-            
-            return config;
-        },
+  e2e: {
+    baseUrl: "https://julianoaleixo.dev",
+    specPattern: "cypress/e2e/**/*.feature",
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: true,
+      json: true,
+      charts: true,
     },
+    async setupNodeEvents(
+      on: Cypress.PluginEvents,
+      config: Cypress.PluginConfigOptions,
+    ) {
+      on = fixCypressOnFix(on);
+
+      require("cypress-mochawesome-reporter/plugin")(on);
+
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        }),
+      );
+
+      return config;
+    },
+  },
 });
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,3 +1,4 @@
 // Support file, loaded before each test.
 // Good place for custom commands and global configuration.
+import "cypress-mochawesome-reporter/register";
 import "./commands";
